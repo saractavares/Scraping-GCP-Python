b53f44e7d13577b2c55fb6ce175e5f1c8bb99cfa
sara <sara27082011@gmail.com>
2022-01-11 18:36:01 -0300

    add script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "by github.com/eduumach"
+sleep 5
+
+echo "criando venv"
+sleep 2
+python3 -m venv scrap-env
+source scrap-env/bin/activate
+clear
+
+echo "baixando requirements"
+sleep 2
+pip install -r requirements.txt
+clear
+
+echo "rodando projeto"
+sleep 2
+python3 main.py
+clear
+
+echo "tudo pronto"
+
