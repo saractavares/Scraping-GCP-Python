e0abee548262d889e7b52d0a2a085a00f42f395b
sara <sara27082011@gmail.com>
2022-01-12 15:01:04 -0300

    update script

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "criando venv"
+echo "Building Venv"
 sleep 2
 python3 -m venv scrap-env
 source scrap-env/bin/activate
 clear
 
-echo "baixando requirements"
+echo "Runing Requirements and Installing Packages"
 sleep 2
 pip install -r requirements.txt
 clear
 
-echo "rodando projeto"
+echo "Runing Work Pipeline"
 sleep 2
 python3 main.py
 clear
 
-echo "tudo pronto"
+echo "Finish"
 
